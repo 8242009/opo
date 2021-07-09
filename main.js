5df077d1f44fe567117570113f07547ac312c4f8
8242009 <81540565+8242009@users.noreply.github.com>
2021-07-09 10:23:41 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+ block_image_width=700;
+block_image_height=510;
+
+
+
+function playSound(){
+	x.play();
+}
